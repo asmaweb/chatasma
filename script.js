facff6794298de0037c6a1cdbba10f38737a9089
asmaq1 <68742642+asmaq1@users.noreply.github.com>
2021-09-19 09:06:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-  // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
 import * as rtdb from "https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js";
@@ -73,4 +72,3 @@ const deleteChat = () =>{
 
 submitChat.addEventListener("click", sendChat);
 deleteButton.addEventListener("click", deleteChat);
-
